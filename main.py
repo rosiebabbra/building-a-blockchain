8bb9a3281e9ebc41cda90404495a55ef0a86942d
Rosie Babbra <rosie.babbra@nielsen.com>
2020-02-09 16:00:48 -0800

    clean

--- main.py
+++ main.py
@@ -3,9 +3,3 @@ from Block import Block
 blockchain = []
 
 genesis_block = Block("Chancellor on the brink...", ['_____ sent 1 BTC to _____'])
-
-print(genesis_block.block_hash)
-
-second_block = Block(genesis_block.block_hash, ['_____ sent 1 BTC to _____'])
-
-print(second_block.block_hash)
